208852eac333e4c3923069749b654dc7e11189c7
gshivach <gshivakumar@companieshouse.gov.uk>
2026-04-28 16:33:15 +0100

    added healthcheck configs

--- terraform/groups/ecs-service/variables.tf
+++ terraform/groups/ecs-service/variables.tf
@@ -127,3 +127,23 @@ variable "disqualified_officers_search_consumer_version" {
   type        = string
   description = "The version of the disqualified_officers_search_consumer container to run."
 }
+
+variable "task_healthcheck_interval" {
+  type        = number
+  description = "Health check interval configuration for ECS task definitions."
+}
+
+variable "task_healthcheck_timeout" {
+  type        = number
+  description = "Health check timeout configuration for ECS task definitions."
+}
+
+variable "task_healthcheck_retries" {
+  type        = number
+  description = "Health check retries configuration for ECS task definitions."
+}
+
+variable "task_healthcheck_start_period" {
+  type        = number
+  description = "Health check start period configuration for ECS task definitions."
+}
